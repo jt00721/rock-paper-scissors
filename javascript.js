72fe9968217a09de7870c434199ebd9f52d514ab
jt00721 <titusgloverj@gmail.com>
2022-06-10 20:44:52 +0100

    Added playRound function

--- javascript.js
+++ javascript.js
@@ -10,4 +10,24 @@ function computerPlay(){
     }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    let playerOption = playerSelection.toLowerCase();
+    console.log('Player ' + playerOption)
+    let computerOption = computerSelection.toLowerCase();
+    console.log('Computer ' + computerOption)
+    if(playerOption == computerOption){
+        return 'Draw!'
+    } else if(playerOption == 'rock' && computerOption == 'paper'){
+        return 'You Lose! Paper beats Rock';
+    } else if(playerOption == 'paper' && computerOption == 'scissors'){
+        return 'You Lose! Scissors beats Paper';
+    } else if(playerOption == 'scissors' && computerOption == 'rock'){
+        return 'You Lose! Scissors beats Paper';
+    } else {
+        return 'You Win!';
+    }
+}
+  
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
